1011e0a07ce170646c5909911bd6b9d42868a917
Will Finlayson <will@digitalstock.co.nz>
2017-12-27 21:59:05 +1300

    init

---
+++ CTNAPI/Controllers/BountiesController.cs
@@ -0,0 +1,42 @@
+﻿using CTNAPI.Models;
+using CTNAPI.Models.View;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Net.NetworkInformation;
+using System.Threading.Tasks;
+using System.Web.Http;
+
+namespace CTNAPI.Controllers
+{
+    [RoutePrefix("api/bounty")]
+    public class BountiesController : ApiController
+    {
+        [Route]
+        [HttpPost]
+        public IHttpActionResult Post(BountyClaimPostModel model)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            ApplicationDbContext db = new ApplicationDbContext();
+
+            // Check to make sure the url does not already exist
+            if (db.BountyClaims.Where(m => m.URL == model.Url).Count() > 0)
+            {
+                return StatusCode(HttpStatusCode.Conflict);
+            }
+
+            // Add the new claim to the database
+            db.BountyClaims.Add(new BountyClaim(model));
+
+            db.SaveChanges();
+
+            return Ok();
+        }
+    }
+}
---
+++ CTNAPI/Models/BountyClaim.cs
@@ -0,0 +1,55 @@
+﻿using CTNAPI.Models.View;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace CTNAPI.Models
+{
+    /// <summary>
+    /// Bounty Model
+    /// </summary>
+    public class BountyClaim
+    {
+        /// <summary>
+        /// The Id for the claim
+        /// </summary>
+        public string Id { get; set; } = Guid.NewGuid().ToString();
+
+        /// <summary>
+        /// The email of the user making the claim
+        /// </summary>
+        public string Email { get; set; }
+
+        /// <summary>
+        /// The address of the wallet to deposit the tokens to
+        /// </summary>
+        public string WalletAddress { get; set; }
+
+        /// <summary>
+        /// The URL of the post to check
+        /// </summary>
+        public string URL { get; set; }
+
+        /// <summary>
+        /// The time the claim was made
+        /// </summary>
+        public DateTime TimeStamp { get; set; } = DateTime.UtcNow;
+
+        /// <summary>
+        /// Required public empty constructor
+        /// </summary>
+        public BountyClaim() { }
+
+        /// <summary>
+        /// Constructor based on post model
+        /// </summary>
+        /// <param name="post">The model post from the front end</param>
+        public BountyClaim(BountyClaimPostModel post)
+        {
+            Email = post.Email;
+            WalletAddress = post.Wallet;
+            URL = post.Url;
+        }
+    }
+}
